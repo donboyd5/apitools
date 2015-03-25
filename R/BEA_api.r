4fc6acffb3235bb401eaaa81bf71475533ad679e
donboyd5 <donboyd5@gmail.com>
2015-03-25 09:32:56 -0400

    Full update

--- R/BEA_api.r
+++ R/BEA_api.r
@@ -14,15 +14,13 @@
 #
 #   Year=X gets all years
 
-# BEA_url()
-# BEA_defaultkey()
 
 #' @title Get a List of Datasets Available from the BEA API
 #'
 #' @description
-#' \code{BEA_DSlist} returns a data frame with a list of available data sets
+#' \code{BEA_DSlist} returns a data frame with a list of available datasets
 #'
-#' @usage BEA_DSlist(yourbeakeyhere)
+#' @usage BEA_DSlist(your_bea_key)
 #' @param key Your BEA API key (can be obtained for free - check www.bea.gov)
 #' @details
 #' Queries the BEA API to get a list of available datasets
@@ -44,9 +42,11 @@ BEA_DSlist <- function(key=BEA_defaultkey()){
 #' @title Get Parameters for a Particular BEA Dataset
 #'
 #' @description
-#' \code{BEA_DSparams} returns a data frame with a list of available data sets
+#' \code{BEA_DSparams} returns a data frame with a list of parameters for the particular dataset
+#' Pay particular attention to whether a data set will accept multiple table flags, or whether tables must be
+#' retrieved one by one
 #'
-#' @usage BEA_DSparams(dsname, yourbeakeyhere)
+#' @usage BEA_DSparams(dsname, your_bea_key)
 #' @param dsname text name of the dataset (see \code{BEA_DSlist})
 #' @param key Your BEA API key (can be obtained for free - check www.bea.gov)
 #' @details
@@ -65,7 +65,8 @@ BEA_DSlist <- function(key=BEA_defaultkey()){
 #' bea.param <- BEA_DSparams("NIPA") # don't need to give it a key if you have yours set
 #' BEA_DSparams("NIUnderlyingDetail")
 BEA_DSparams <- function(dsname, key=BEA_defaultkey()) {
-  # get parameters for a given bea dataset
+  require(RCurl)
+  require(jsonlite)
   url <- paste0(BEA_url(), "?&UserID=", key, "&method=GetParameterList&datasetname=", dsname,"&")
   result <- getURL(url, .opts=curlOptions(followlocation=TRUE)) # get character var with all the info, in JSON format
   df <- fromJSON(result)$BEAAPI$Results$Parameter # returns a data frame
@@ -73,23 +74,39 @@ BEA_DSparams <- function(dsname, key=BEA_defaultkey()) {
 }
 
 
-#
-# # getBEA_DSparams("NIPA") # does NOT accept multiple table flags
-# # getBEA_DSparams("NIUnderlyingDetail") # does NOT accept multiple table flags
-#
-#
+
 #
 # #  Get parameter values for a parameter, for each data set -- IMPORTANT for the parameter TableID ####
-# getBEA_ParamVals <- function(dsname, pname, key=beaapikey){
-#   url <- paste0("http://www.bea.gov/api/data?&UserID=", key,
-#                 "&method=GetParameterValues&datasetname=", dsname,
-#                 "&ParameterName=", pname,"&")
-#   result <- getURL(url, .opts=curlOptions(followlocation=TRUE)) # get character var with all the info, in JSON format
-#   df <- fromJSON(result)$BEAAPI$Results$ParamValue # returns a data frame
-#   return(df)
-# }
+#' @title Get Parameter Values for a Parameter in a Chosen BEA Dataset
+#'
+#' @description
+#' \code{BEA_ParamVals} returns a data frame with a list of parameters for the particular dataset
+#' Pay particular attention to whether a data set will accept multiple table flags, or whether tables must be
+#' retrieved one by one
+#'
+#' @usage BEA_ParamVals(dsname, yourbeakeyhere)
+#' @param dsname text name of the dataset (e.g., "NIPA") (see \code{BEA_DSlist})
+#' @param pname text name of the parameter of interest (e.g., "TableID" for the dataset "NIPA")  (see \code{BEA_DSparams})
+#' @param key Your BEA API key (can be obtained for free - check www.bea.gov)
+#' @details
+#' Queries the BEA API to get allowable values for a parameter of interest for a dataset of interest. Particularly useful datasets are NIPA and NIUnderlyingDetail
+#' @return data frame with information about allowable values for the parameter of interest
+#' @keywords BEA_ParamVals
+#' @export
+#' @examples
+#' nipa.tablist <- BEA_ParamVals("NIPA", "TableID") # don't need to give a key if you have yours set
+#' head(nipa.tablist)
+#' nipa.uld.tablist <- BEA_ParamVals("NIUnderlyingDetail", "TableID") # don't need to give a key if you have yours set
+#' head(nipa.uld.tablist)
+BEA_ParamVals <- function(dsname, pname, key=BEA_defaultkey()) {
+  require(RCurl)
+  require(jsonlite)
+  url <- paste0(BEA_url(), "?&UserID=", key,
+                "&method=GetParameterValues&datasetname=", dsname,
+                "&ParameterName=", pname,"&")
+  result <- getURL(url, .opts=curlOptions(followlocation=TRUE)) # get character var with all the info, in JSON format
+  df <- fromJSON(result)$BEAAPI$Results$ParamValue # returns a data frame
+  return(df)
+}
 
-# Look at table lists to find needed tables
-# nipatlist <- getBEA_ParamVals("NIPA", "TableID")
-# niudtlist <- getBEA_ParamVals("NIUnderlyingDetail", "TableID")
 
