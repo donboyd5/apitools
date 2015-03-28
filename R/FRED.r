53c26b8e96a30c913aa882317d0104d83f514c11
donboyd5 <donboyd5@gmail.com>
2015-03-28 12:44:02 -0400

    Added FREDcat

--- R/FRED.r
+++ R/FRED.r
@@ -57,3 +57,76 @@ FRED <- function(svec, key=FRED_defaultkey(), full=FALSE) {
   return(df2)
 }
 
+
+#' @title Get info on variables in one or more FRED categories
+#'
+#' @description
+#' \code{FREDcat} returns a data frame with info on series
+#'
+#' @usage FREDcat(catvec, key)
+#' @param catvec numeric vector with list of FRED categories to get info for
+#' @param key Your FRED API key (can be obtained for free - check http://api.stlouisfed.org/api_key.html)
+#' default returns data frame with info on series available in one or several categories
+#' @details
+#' Queries the FRED API to get the requested data
+#' @return data frame
+#' @keywords FREDcat
+#' @export
+#' @examples
+#' require(dplyr)
+#' df <- FREDcat(c(106, 125))
+#' df <- FREDcat(18) # 18 is NIPA, so this will get info on ALL series in the NIPA category and its children
+#' df2 <- df %>% filter(grepl("Billions of Chained 2009 Dollars", units),
+#'               grepl("consumption", title, ignore.case=TRUE),
+#'               grepl("durable", title, ignore.case=TRUE)) %>%
+#'        select(cat, id, title, frequency_short, seasonal_adjustment_short, units_short)
+#' # Look at df2 in the viewer to find the desired consumption variables
+#' # Let's look at PCDGCC96 real quarterly durables, and PCNDGC96 real q nondurables
+#' head(df)
+#' count(df, id, title)
+#' vars <- c("PCDGCC96", "PCNDGC96")
+#' FRED(vars)
+FREDcat <- function(catvec, key=FRED_defaultkey()) {
+  # get info on all series in all categories and children of those categories in catvec
+  fredroot <- "http://api.stlouisfed.org/fred/"
+  fredpost <- paste0("&api_key=", key, "&file_type=json")
+
+  getonecategory <- function(cat) {
+    # get info on all series in one category
+    catinfo <- paste0("category/series?category_id=", cat)
+    url <- paste0(fredroot, catinfo, fredpost)
+    result <- jsonlite::fromJSON(url)
+    df <- data.frame(result$seriess) %>% select(-contains("realtime"))
+    return(df)
+  }
+
+  getallcatchildren <- function(catvec) {
+    # get vector of all the children categories for each category in catvec
+    getonecatschildren <- function(cat) {
+      url <- paste0(fredroot, "category/children?category_id=", cat, fredpost)
+      df2 <- data.frame(jsonlite::fromJSON(url))
+      if(nrow(df2)==0) df2 <- data.frame(categories.id=cat)
+      return(df2)
+    }
+    # get vector with all children of all categories in catvec
+    df <- data.frame(cat=catvec) %>%
+      group_by(cat) %>%
+      do(getonecatschildren(.$cat))
+
+    catout <- unique(df$categories.id)
+    return(catout)
+  }
+
+  # Here is where the real work gets done
+  catvec2 <- getallcatchildren(catvec) # get the full category vector
+  # now get info on all series in the full category vector
+  df2 <- data.frame(cat=catvec2) %>%
+    group_by(cat) %>%
+    do(getonecategory(.$cat))
+
+  return(df2)
+}
+
+
+
+
